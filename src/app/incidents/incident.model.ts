71ce341ae4f33a415537d36159fb10b1ff0ca7a8
philm <philmayfield@gmail.com>
2018-09-30 19:06:46 -0700

    set default values on the model

--- src/app/incidents/incident.model.ts
+++ src/app/incidents/incident.model.ts
@@ -1,9 +1,9 @@
 export class Incident {
   constructor(
-    public id: number,
-    public name: string,
-    public discovered: string,
-    public description: string,
-    public department: string
+    public id: number = -1,
+    public name: string = '',
+    public discovered: string = '',
+    public description: string = '',
+    public department: string = ''
   ) {}
 }
