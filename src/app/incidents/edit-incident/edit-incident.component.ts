00d29cd95315e5105bdd1acdc51c030df08905bb
philm <philmayfield@gmail.com>
2018-10-01 17:10:06 -0700

    Revert "utilize date pipe to format date string rather than doing it manually, bind max property"
    This reverts commit 2b5893f80dd3406588e9e412cb11d10cb96e4b6a.

--- src/app/incidents/edit-incident/edit-incident.component.ts
+++ src/app/incidents/edit-incident/edit-incident.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { IncidentsService } from '../incidents.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Incident } from '../incident.model';
-import { DatePipe, Location } from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-edit-incident',
   templateUrl: './edit-incident.component.html',
-  styleUrls: ['./edit-incident.component.css'],
-  providers: [DatePipe]
+  styleUrls: ['./edit-incident.component.css']
 })
 export class EditIncidentComponent implements OnInit {
   incident: Incident;
@@ -19,12 +18,17 @@ export class EditIncidentComponent implements OnInit {
   constructor(private incService: IncidentsService,
               private route: ActivatedRoute,
               private router: Router,
-              private location: Location,
-              private datePipe: DatePipe) { }
+              private location: Location) { }
 
   ngOnInit() {
     const {id} = this.route.snapshot.params;
-    const today = this.datePipe.transform(new Date().toLocaleDateString(), 'yyyy-MM-dd');
+    const todayString = new Date().toLocaleDateString();
+    let [month, day, year] = todayString.split('/');
+    month = month.length === 1 ? `0${month}` : month;
+    day = day.length === 1 ? `0${day}` : day;
+    year = year; // linting was throwing an error without re-assigning
+    const today = `${year}-${month}-${day}`;
+
     this.today = today;
 
     if (id && id === 'new') {
